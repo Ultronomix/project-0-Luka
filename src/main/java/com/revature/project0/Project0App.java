dde9989814b95aa5bf25af082121f346f68df146
Luka Licheli <lukalicheli@gmail.com>
2022-08-28 02:51:11 -0700

    Established a connection

--- src/main/java/com/revature/project0/Project0App.java
+++ src/main/java/com/revature/project0/Project0App.java
@@ -1,8 +1,29 @@
 package com.revature.project0;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.io.IOException;
+
 public class Project0App {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String dbUrl = "jdbc:postgresql://project0.cest4uy9wjqq.us-west-1.rds.amazonaws.com:5432/";
+        String dbUsername = "postgres";
+        String dbPassword = "revature";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            if (conn != null) {
+                System.out.println("Connection Successful");
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("Could not find Postgresql jbdc driver, connection attempt aborted.");
+        } catch (SQLException e) {
+            System.err.println("Could not establish a connection");
+
+        }
+
     }
 
 }
